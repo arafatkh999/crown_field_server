edf364a8e8342abd85d97a8ce1f6a5672e81a2be
arafatkh999 <arafatkh999@gmail.com>
2023-06-09 18:28:58 +0600

    all users get api created

--- index.js
+++ index.js
@@ -36,8 +36,18 @@ async function run() {
     const usersCollection = client.db("crownDb").collection("users");
 
 
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res)=>{
       const user = req.body;
+      const query = { email : user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({ message: 'user already exists'})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
