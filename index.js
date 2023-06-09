79a8ad0851183bb2dc35b1c85b444d3654be312e
arafatkh999 <arafatkh999@gmail.com>
2023-06-10 00:58:40 +0600

    jwt created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +35,13 @@ async function run() {
     const classesCollection = client.db("crownDb").collection("classes");
     const cartCollection = client.db("crownDb").collection("carts");
     const usersCollection = client.db("crownDb").collection("users");
+
+
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+      res.send({token});
+    })
 
 
     app.get('/users', async(req, res)=>{
